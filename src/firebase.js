4364296afe2f25c3b4c675e22a167f8c109bcf59
raartist <rishabhzee98@gmail.com>
2020-11-11 15:06:37 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDQ92PExvjqpGEkd_xuJ4PucPCFrEqLQ-8",
+    authDomain: "whatsapp-clone-14f61.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-14f61.firebaseio.com",
+    projectId: "whatsapp-clone-14f61",
+    storageBucket: "whatsapp-clone-14f61.appspot.com",
+    messagingSenderId: "698650303376",
+    appId: "1:698650303376:web:81ae4c1a683621d00db115",
+    measurementId: "G-JMT8JF1FF1"
+};
